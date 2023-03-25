9cdf00b0ae2c5ec469c99e29f163070984b398da
Luka Salaj <lukasalaj9@gmail.com>
2023-03-25 23:40:58 +0100

    Dodan popup za zvuk
    Potrgo animaciju

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,20 +1,27 @@
-﻿namespace Dinory;
+﻿using CommunityToolkit.Maui.Extensions;
+using CommunityToolkit.Maui.Views;
+
+namespace Dinory;
 
 public partial class MainPage : ContentPage
 {
 
-	public MainPage()
-	{
-		InitializeComponent();
+    public MainPage()
+    {
+        InitializeComponent();
 
-	}
+    }
     private void OnClickStartGame(object sender, EventArgs e)
     {
-         Navigation.PushAsync(new DifficultyPage());
+        Navigation.PushAsync(new DifficultyPage());
     }
     private void OnClickCloseGame(object sender, EventArgs e)
     {
         Application.Current.Quit();
     }
-}
 
+    private void ButtonClicked(object sender, EventArgs e)
+    {
+        this.ShowPopupAsync(new PopupPage());
+    }
+}
--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using CommunityToolkit.Maui;
+using Microsoft.Extensions.Logging;
 
 namespace Dinory;
 
@@ -9,7 +10,8 @@ public static class MauiProgram
 		var builder = MauiApp.CreateBuilder();
 		builder
 			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
+            .UseMauiCommunityToolkit()
+            .ConfigureFonts(fonts =>
 			{
                 fonts.AddFont("Poppins-Regular.ttf", "PoppinsRegular");
                 fonts.AddFont("Poppins-SemiBold.ttf", "PoppinsSemiBold");
