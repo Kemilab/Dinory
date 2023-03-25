1fdec476e88cdb4d0990ca3bf6759739677394da
Rubinich <alojzrubinic@gmail.com>
2023-03-25 21:52:15 +0100

    Difficulty page update v3
    Button animations, finished organization

--- DifficultyPage.xaml.cs
+++ DifficultyPage.xaml.cs
@@ -6,9 +6,28 @@ public partial class DifficultyPage : ContentPage
 	{
 		InitializeComponent();
 	}
-    private void OnClickReturnPage(object sender, EventArgs e)
+	private async void OnClickReturnPage(object sender, EventArgs e)
 	{
-		 Navigation.PopAsync();
+		var button = sender as ImageButton;
+		await button.ScaleTo(1.5, 100);
+		await Task.Delay(200);
+		await button.ScaleTo(1, 100);
+        await Task.Delay(200);
+        await Navigation.PopAsync();
 	}
-    
+    public static async Task OnClickButtonAnimation(object sender, EventArgs e)
+    {
+        Button button = (Button)sender;
+        await button.ScaleTo(0.9, 100);
+        await Task.Delay(200);
+        await button.ScaleTo(1, 100);
+        await Task.Delay(200);
+    }
+	private async void OnClickDifficultyEasy(object sender, EventArgs e)
+	{
+		await DifficultyPage.OnClickButtonAnimation(sender, e);
+		//samo za primjer
+        await Navigation.PopAsync();
+    }
+
 }
