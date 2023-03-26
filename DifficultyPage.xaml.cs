54df76c3d7b9458a67703321bb6bd0382cb0bf61
Rubinich <alojzrubinic@gmail.com>
2023-03-26 11:12:47 +0200

    Update DiffficultyPage v3
    more usable animation functions

--- DifficultyPage.xaml.cs
+++ DifficultyPage.xaml.cs
@@ -6,26 +6,34 @@ public partial class DifficultyPage : ContentPage
 	{
 		InitializeComponent();
 	}
-	private async void OnClickReturnPage(object sender, EventArgs e)
-	{
-		var button = sender as ImageButton;
-		await button.ScaleTo(1.5, 100);
-		await Task.Delay(200);
-		await button.ScaleTo(1, 100);
-        await Task.Delay(200);
-        await Navigation.PopAsync();
-	}
-    public static async Task OnClickButtonAnimation(object sender, EventArgs e)
+
+    public static async Task OnClickButtonBig(ImageButton ImageButton, int scaleDuration, int delayDuration)
     {
-        Button button = (Button)sender;
-        await button.ScaleTo(0.9, 100);
-        await Task.Delay(200);
-        await button.ScaleTo(1, 100);
-        await Task.Delay(200);
+        await ImageButton.ScaleTo(1.3, (uint)scaleDuration);
+        await Task.Delay(delayDuration);
+        await ImageButton.ScaleTo(1, (uint)scaleDuration);
+        await Task.Delay(delayDuration);
     }
+
+    public static async Task OnClickButtonSmall(Button Button, int scaleDuration, int delayDuration)
+    {
+        await Button.ScaleTo(0.9, (uint)scaleDuration);
+        await Task.Delay(delayDuration);
+        await Button.ScaleTo(1, (uint)scaleDuration);
+        await Task.Delay(delayDuration);
+    }
+
+    private async void OnClickReturnPage(object sender, EventArgs e)
+	{
+        ImageButton button = (ImageButton)sender;
+        await OnClickButtonBig(button, 100, 200);
+        await Navigation.PopAsync();
+    }
+    
 	private async void OnClickDifficultyEasy(object sender, EventArgs e)
 	{
-		await DifficultyPage.OnClickButtonAnimation(sender, e);
+		Button button = (Button)sender;
+        await OnClickButtonSmall(button, 100, 200);
 		//samo za primjer
         await Navigation.PopAsync();
     }
