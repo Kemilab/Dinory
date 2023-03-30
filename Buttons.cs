e9f745256cc079aff8bfacf6c4b597d7296ae65e
Rubinich <alojzrubinic@gmail.com>
2023-03-30 20:39:37 +0200

    Update Buttons.cs

--- Buttons.cs
+++ Buttons.cs
@@ -9,5 +9,19 @@
             await Button.ScaleTo(1, (uint)scaleDuration);
             await Task.Delay(delayDuration);
         }
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            countdownCancellationTokenSource.Cancel(); // Stop the countdown when the user leaves the page
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            if (countdownCancellationTokenSource.IsCancellationRequested)
+            {
+                _ = StartCountdown(45); // Restart the countdown when the user re-enters the page
+            }
+        }
     }
 }
